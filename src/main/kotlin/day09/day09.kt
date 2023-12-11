dac813e4d1b359edd0a2a8133e7e279f4b80046d
Karoly Machalik <karoly_machalik@epam.com>
2023-12-11 11:00:54 +0100

    Day 09

--- src/main/kotlin/day09/day09.kt
+++ src/main/kotlin/day09/day09.kt
@@ -10,8 +10,30 @@ val INPUT_FILE_NAME = "src/main/resources/day$DAY/input${if (TEST == 0) "" else 
 
 fun main() {
     val lines = File(INPUT_FILE_NAME).readLines()
+    val sequences = lines.map { line -> line.split(' ').map { it.trim().toInt() } }
 
-//    println("Task01: $task01")
+    val result = sequences.map { intList ->
+        val firstAndLastElements = mutableListOf<Pair<Int, Int>>()
+        var test = intList
+        while (test.any { it != 0 }) {
+            firstAndLastElements.add(test.first() to test.last())
+            test = (0 until test.size - 1).map { test[it + 1] - test[it] }
+        }
 
-//    println("Task02: $task02")
+        val intProgression = (firstAndLastElements.size - 1) downTo 0
+        val extrapolatedNext = intProgression.sumOf { firstAndLastElements[it].second }
+
+        var extrapolatedPrior = 0
+        intProgression.map { index ->
+            extrapolatedPrior = firstAndLastElements[index].first - extrapolatedPrior
+        }
+
+        extrapolatedNext to extrapolatedPrior
+    }
+
+    val task01 = result.sumOf { it.first }
+    println("Task01: $task01")
+
+    val task02 = result.sumOf { it.second }
+    println("Task02: $task02")
 }
